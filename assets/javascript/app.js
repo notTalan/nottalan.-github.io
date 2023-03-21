7491096ceb1fcce54df89089b09964ae955c59e8
talan <128263833+notTalan@users.noreply.github.com>
2023-03-20 21:03:34 -0700

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.5;
+  musicFadeIn = 2000;
+  skippedIntro = false;
+  backgroundToggler = false;
+  shouldIgnoreVideo = false;
+  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  brandDescription = ['drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', 'drugs', 'money', 'thug shakers', 'etc', ];
+  
+}
+
+const app = new _app();
