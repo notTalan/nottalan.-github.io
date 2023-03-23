4bd37d7cd48b69837ae7997f2b41fefef1566ba8
talan <128263833+notTalan@users.noreply.github.com>
2023-03-22 23:48:16 -0700

    Bug fixes
    - Updated Socials and Back to main buttons

---
+++ socials/assets/javascript/app.js
@@ -0,0 +1,23 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.5;
+  musicFadeIn = 2000;
+  backgroundToggler = false;
+  shouldIgnoreVideo = false;
+  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  brandDescription = ['snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', 'snapchat.com/add/talan.png', 'snapchat - talan.png', '👻 snapchat.com/add/talan.png', ];
+  brandDescription2 = ['instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan', 'instagram - ihy.talan', 'instagram.com/ihy.talan'];
+  brandDescription3 = ['twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', 'twitter - villeck_', 'twitter.com/villeck_', ];
+  brandDescription4 = ['steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', 'steam/id - villeck', 'steamcommunity.com/id/villeck', ];
+  brandDescription5 = ['twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', 'twitch - villeck', 'twitch.tv/villeck', ];
+  brandDescription6 = ['spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', 'spotify -  niagara', 'open.spotify.com/user/n%C3%ACagara?si=0fb2e91273714811&nd=1', ];
+  
+}
+
+const app = new _app();
---
+++ socials/assets/javascript/dick.js
@@ -0,0 +1,7 @@
+(function titleScroller(text) {
+  document.title = text;
+  setTimeout(function () {
+    titleScroller(text.substr(1) + text.substr(0, 1));
+  }, 350);
+  
+}("everyone hates talan ⛧ "));
